a0bc4c764fa2b73ad965128323237d1b8899dc76
王臻彦 <wangzhenyan@herenit.com>
2023-02-23 15:49:24 +0800

    支持反射获取方法中返回的配置信息 支持.net 6.0

---
+++ src/redis4net.NETFramework/Util/Encryption/DESEncryptor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redis4net.Util.Encryption
+{
+    public class DESEncryptor
+    {
+        private const string ENCRYPT_KEY = "Heren1T~-Te0h&@-+`.!";
+        private static StringBuilder _key = new StringBuilder();
+        public static string Key
+        {
+            get
+            {
+                if (_key.Length == 0)
+                {
+                    for (int i = 0; i < ENCRYPT_KEY.Length; i += 2)
+                    {
+                        _key.Append(ENCRYPT_KEY[i]);
+                        if (_key.Length == 8) break;
+                    }
+                }
+                return _key.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 进行DES加密。
+        /// </summary>
+        /// <param name="pToEncrypt">要加密的字符串。</param>
+        /// <returns>以Base64格式返回的加密字符串。</returns>
+        public static string Encrypt(string pToEncrypt)
+        {
+            return Encrypt(pToEncrypt, Key);
+        }
+
+        /// <summary>
+        /// 进行DES解密。
+        /// </summary>
+        /// <param name="pToDecrypt">要解密的以Base64</param>
+        /// <returns>已解密的字符串。</returns>
+        public static string Decrypt(string pToDecrypt)
+        {
+            try
+            {
+                return Decrypt(pToDecrypt, Key);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 进行DES加密。
+        /// </summary>
+        /// <param name="pToEncrypt">要加密的字符串。</param>
+        /// <param name="sKey">密钥，且必须为8位。</param>
+        /// <returns>以Base64格式返回的加密字符串。</returns>
+        public static string Encrypt(string pToEncrypt, string sKey)
+        {
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(pToEncrypt);
+                des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(inputByteArray, 0, inputByteArray.Length);
+                    cs.FlushFinalBlock();
+                }
+                string str = Convert.ToBase64String(ms.ToArray());
+                ms.Close();
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// 进行DES解密。
+        /// </summary>
+        /// <param name="pToDecrypt">要解密的以Base64</param>
+        /// <param name="sKey">密钥，且必须为8位。</param>
+        /// <returns>已解密的字符串。</returns>
+        public static string Decrypt(string pToDecrypt, string sKey)
+        {
+            byte[] inputByteArray = Convert.FromBase64String(pToDecrypt);
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(inputByteArray, 0, inputByteArray.Length);
+                    cs.FlushFinalBlock();
+                }
+                string str = Encoding.UTF8.GetString(ms.ToArray());
+                ms.Close();
+                return str;
+            }
+        }
+    }
+}
---
+++ src/redis4net.NETFramework/Util/Encryption/EncryptionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redis4net.Util.Encryption
+{
+    public static class EncryptionExtensions
+    {
+        public static string Encrypt(this string str)
+        {
+            return DESEncryptor.Encrypt(str);
+        }
+
+        public static string Decrypt(this string str, bool isForce = false)
+        {
+            string decryptStr = DESEncryptor.Decrypt(str);
+
+            if (string.IsNullOrEmpty(decryptStr) && !string.IsNullOrEmpty(str))
+            {
+                if (isForce)
+                    return null;
+                else
+                    return str;
+            }
+            else
+            {
+                return decryptStr;
+            }
+        }
+
+    }
+}
